452bb3d4f1c2b85255d53fbe1b4964719cbf3404
Himanshu tyagi <himanshutyagi0158@gmail.com>
2025-06-01 07:01:42 +0530

    minor UI enhancements

--- frontend/src/Pages/Main-Pages/Company/Overview.jsx
+++ frontend/src/Pages/Main-Pages/Company/Overview.jsx
@@ -1,13 +1,14 @@
 import '../../../Styles/Pages/Main-Pages/Company/Overview.css';
 import ReactMarkdown from 'react-markdown';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getOverviewDataFunc } from '../../../Redux/MainReducer/action';
 import { useParams } from 'react-router-dom';
 
 export const Overview = () => {
     const { compId } = useParams();
-    const { data: { overview, stats } } = useSelector(store => store.mainReducer);
+    const [fullOverview, setFullOverview] = useState(false);
+    const { data: { overview, stats }, selectedCompany } = useSelector(store => store.mainReducer);
     const dispatch = useDispatch();
     const processStatsData = (key, statCategory) => {
         const { columns, values } = stats[key];
@@ -16,7 +17,7 @@ export const Overview = () => {
         }
         return (
             <>
-                <span className='stat-category'>{statCategory}</span>
+                <h3 className='stat-category'>{statCategory}</h3>
                 {
                     values.map(value => {
                         return (
@@ -30,19 +31,26 @@ export const Overview = () => {
             </>
         )
     }
+    const splitOverview = (text = '') => {
+        const cleaned = text.replace(new RegExp(`^For company ${selectedCompany.companyName} Company Overview Hide\\s*\\n*\\s*`, 'i'), '');
+        const [intro = '', ...rest] = cleaned.split(/\n{2,}/);
+        return intro.trim() + (fullOverview ? '\n\n' + rest.join('\n\n') : '');
+    };
     useEffect(() => {
         dispatch(getOverviewDataFunc(compId))
     }, [compId, dispatch])
     return (
         <div className='overview-container'>
             <div className='overview'>
-                <ReactMarkdown>{overview?.overview_text}</ReactMarkdown>
-                <button className='overview-show-more'>Show More...</button>
+                <h2 className='overview-heading'>Company Overview</h2>
+                <ReactMarkdown>{splitOverview(overview?.overview_text)}</ReactMarkdown>
+                <button className='overview-show-more' onClick={() => setFullOverview(!fullOverview)}>{fullOverview ? 'Show Less' : 'Show More'}</button>
             </div>
             <div className='chart'>
 
             </div>
             <div className='statistics'>
+                <h2 className='stats-heading'>Company Statistics</h2>
                 <div className="profile">{stats && processStatsData("profile_metrics", 'Profile')}</div>
                 <div className="margins">{stats && processStatsData("margins_metrics", 'Margins')}</div>
                 <div className="returns-5yr">{stats && processStatsData("returns_5yr_avg_metrics", 'Returns (5Yr Avg)')}</div>
